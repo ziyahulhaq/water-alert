57afdb9b8589151cda5fea45a2c2a63d397d2844
ziyahulhaq <ziyavulhaq94@gmail.com>
2026-06-15 18:36:20 +0530

    push notification

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-a71ad1c7'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.gimkn7jk2bs"
+    "revision": "0.pjkji08dk8c"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
